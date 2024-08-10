278ea0ca66121e6f0d90be5fc90fd886b6e4ed22
Matheus Rafael <matheusrafael77@hotmail.com>
2024-08-10 16:59:42 -0300

    melhoria

--- src/app/table/page.tsx
+++ src/app/table/page.tsx
@@ -1,23 +1,10 @@
 import { TableData } from "@/components/components-person/table-data";
 import { criptoDataResponse } from "@/components/models/criptoData";
-
-async function getCryptoData(): Promise<criptoDataResponse[]> {
-  const page = 1;
-  const limit = 10;
-  const res = await fetch(
-    `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&page=${page}&per_page=${limit}`
-  );
-  if (!res.ok) {
-    throw new Error("Falha ao buscar dados");
-  }
-
-  // atraso
-  const data = await res.json();
-  return data;
-}
+import useCryptoService from "@/services/ctypodate.service";
 
 export default  async function Table() {
-  const data = await getCryptoData();
+  const crypoService = useCryptoService()
+  const data = await crypoService.getCryptoData();
   return (
     <main className="flex flex-col items-center justify-between p-24 gap-10">
       <div className="w-full lg:w-3/6">
---
+++ src/services/ctypodate.service.ts
@@ -0,0 +1,21 @@
+import { criptoDataResponse } from "@/components/models/criptoData";
+
+export default function useCryptoService() {
+  async function getCryptoData(): Promise<criptoDataResponse[]> {
+    const page = 1;
+    const limit = 15;
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&page=${page}&per_page=${limit}`
+    );
+    if (!res.ok) {
+      throw new Error("Falha ao buscar dados");
+    }
+
+    const data = await res.json();
+    return data;
+  }
+  return {
+    getCryptoData
+  }
+
+}
