b4cb8364b1e616f1384d8b83077bf238d3b43652
Matheus Rafael <matheusrafael77@hotmail.com>
2024-08-10 21:35:44 -0300

    remove page

--- src/services/ctypodate.service.ts
+++ src/services/ctypodate.service.ts
@@ -3,9 +3,8 @@ import { criptoDataResponse } from "@/components/models/criptoData";
 export default function useCryptoService() {
   async function getCryptoData(): Promise<criptoDataResponse[]> {
     const page = 1;
-    const limit = 15;
     const res = await fetch(
-      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&page=${page}&per_page=${limit}`
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=brl&page=${page}&per_page=15`,
     );
     if (!res.ok) {
       throw new Error("Falha ao buscar dados");
